d286bed27c2f17f600b09f4c8bd3d9b01b2f9233
Justinas Sinkevicius <36824644+justuss1@users.noreply.github.com>
2024-06-06 00:33:55 +0300

    Update script.js

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000/', {
+    const response = await fetch('https://justuss1-opeai-chat-test-v2.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
